ff3552944af9da55ff14d99251157fda3e69cce5
Vnb123 <vanabg@yahoo.com>
2025-06-10 15:14:51 +0000

    Setting up for hosting, also working on html and css

--- listen.js
+++ listen.js
@@ -1,9 +1,9 @@
 const app = require("./app");
-
-app.listen(7070, (err) => {
+const { PORT = 7070 } = process.env;
+app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("listening on 7070");
+    console.log(`listening on ${PORT}`);
   }
 });
