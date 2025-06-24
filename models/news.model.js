6fa4fd4892ba23753fd3ecedb427cc225e4ac812
Vnb123 <vanabg@yahoo.com>
2025-06-24 14:53:19 +0000

    Fixed comment count not showing up in individual articles

--- models/news.model.js
+++ models/news.model.js
@@ -9,7 +9,10 @@ const selectTopics = () => {
 const selectArticles = (article_id) => {
   if (article_id) {
     return db
-      .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+      .query(
+        `SELECT articles.article_id, title, topic, articles.author, articles.created_at, articles.votes, article_img_url, COUNT (comments.article_id) as comment_count FROM articles LEFT JOIN comments ON (articles.article_id = comments.article_id) WHERE articles.article_id = $1 GROUP BY articles.article_id;`,
+        [article_id]
+      )
       .then(({ rows }) => {
         if (!rows.length) {
           return Promise.reject({ status: 404, msg: "not found" });
